79b1a173c82d6f1befde21b3d05299ec32f73819
uchidasann <pl05367687@163.com>
2020-05-26 16:44:02 +0800

    Update max_heap.cpp

--- data structure/max_heap.cpp
+++ data structure/max_heap.cpp
@@ -5,7 +5,7 @@ class max_heap{
     T heap[MAXNUM];
 public: 
     max_heap():n(0){}
-    void push(T a){
+    void push(T a){         //自底向上，为a寻找一个合适的位置插入
         int pos = ++n, fatherpos = n >> 1;
         while(fatherpos){
             if(a > heap[fatherpos]){
@@ -16,7 +16,7 @@ public:
         }
         heap[pos] = a;
     }
-    void pop(){
+    void pop(){         //弹出顶部元素，自顶向下，为最后一个节点找到合适的地方插入
         T a = heap[n--];
         int root = 1, kid = 2;
         while(kid <= n) {
